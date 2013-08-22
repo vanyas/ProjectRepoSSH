30df9378487187d83ad30febf8a96be09f4c5bea
Vangeli Ontiveros <vanyasescom@gmail.com>
2013-08-22 13:09:15 -0500

    commit desde github for mac

---
+++ SSHTest/SSHTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SSHTest
+//
+//  Created by VANGELI ONTIVEROS on 22/08/13.
+//  Copyright (c) 2013 com.vanyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SSHTest/SSHTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SSHTest
+//
+//  Created by VANGELI ONTIVEROS on 22/08/13.
+//  Copyright (c) 2013 com.vanyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
